19dea9e9cf4857f51deecc4d0be9f86c984e9e19
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-25 13:04:34 +0600

    server side initial setup done

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const cors=require('cors');
+const app = express()
+const port =process.env.PORT || 5000
+
+app.use(cors());
+
+app.get('/', (req, res) => {
+    res.send('server is running')
+  });
+
+  app.listen(port, () => {
+    console.log(`server is running on port ${port}`)
+  })
