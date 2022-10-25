0597abf63b1d07f4bdd823adfe6a1e730874e680
jahidhasan8 <jahidhasan1932@gmail.com>
2022-10-26 00:27:44 +0600

    api link created

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const cors=require('cors');
+const cors = require('cors');
 const app = express()
-const port =process.env.PORT || 5000
+const port = process.env.PORT || 5000
 
 app.use(cors());
 
-const courses=require('./data/courses.json')
+const courses = require('./data/courses.json')
 
 app.get('/', (req, res) => {
     res.send('server is running')
-  });
+});
 
- 
+app.get('/courses', (req, res) => {
+    res.send(courses)
+})
 
-  app.listen(port, () => {
+app.get('/courses/:id', (req, res) => {
+    const id = parseInt(req.params.id)
+    const selectedCourse = courses.find(course => course.id === id);
+    res.send(selectedCourse);
+})
+
+
+app.listen(port, () => {
     console.log(`server is running on port ${port}`)
-  })
+})
